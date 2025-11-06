04cfa08f51c3f1c3da23d1628a587cb7b5419f25
bpdu <42397650+bpdu@users.noreply.github.com>
2025-11-06 10:56:38 +0300

    Implement stat loading, saving, and bot status check
    Added functions to load and save statistics, and check if the bot is running.

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
 import os
+import subprocess
+import json
+from pathlib import Path
+import utils
+
 def load_prompt(name):
     with open(f"resources/{name}.txt", "r", encoding="utf8") as file:
         return file.read()
 
 def load_token(name):
-    # === Load Telegram bot token ===
     dotenv_path = 'secrets/pondsupportbot2/'+name+'.env'
     load_dotenv(dotenv_path)
 
@@ -13,3 +17,26 @@ def load_token(name):
     if not TOKEN:
         raise ValueError(name+' token not found')
     return TOKEN
+
+STAT_FILE = Path("stat/stat.json")
+
+def load_stat():
+    if not STAT_FILE.exists():
+        return {"visitors": 0, "buttons": ["sales": 0, "support": 0, "usage": 0, "coverage": 0]}
+    with open(STAT_FILE, "r") as f:
+        return json.load(f)
+
+def save_stat(stat):
+    STAT_FILE.parent.mkdir(parents=True, exist_ok=True)
+    with open(STAT_FILE, "w") as f:
+        json.dump(stat, f)
+
+def is_bot_running():
+    try:
+        result = subprocess.run(
+            ["systemctl", "is-active", "--quiet", "pondsupportbot.service"],
+            check=False
+        )
+        return result.returncode == 0
+    except Exception:
+        return False
